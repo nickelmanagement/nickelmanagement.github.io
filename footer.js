0cd74f778d6c459d18585d752c708434836a46b4
nickelmanagement <npeters06+nickel@gmail.com>
2017-03-15 07:48:07 -0400

    Uploading 1st draft of full site

---
+++ footer.js
@@ -0,0 +1,69 @@
+document.write('\
+<footer>\
+			<div class="container">\
+				<div class="row">\
+\
+					<!-- Single Footer -->\
+					<div class="col-sm-2">\
+					<img src="img/nickel-logo.png">\
+					</div>\
+					<div class="col-sm-6">\
+						<div class="single-footer">\
+							<div class="footer-logo">\
+								<h3>Nickel Management</h3>\
+								<p>Nickel Management, LLC is a full service property managment company for all your residential and commercial needs.</p>\
+								<p>We specialize in single family homes, small apartment buildings, and small commercial buildings in the Northern Hills area. Contact us for a free rent analysis and management consultation.</p>\
+							</div>\
+						</div>\
+					</div>\
+					<!-- Single Footer -->\
+\
+					<!-- Single Footer -->\
+					<div class="col-sm-4">\
+						<div class="single-footer">\
+							<h4>Contact</h4>\
+							<p>638 N Main St, Suite B, Spearfish, SD<br/>\
+							(Inside Good Earth Natural Foods)<br/>\
+							+1 (605) 642-8400 <br />\
+							<a style="color:#f8f8f8" href="mailto:info@nickelmanagement.com">info@nickelmanagement.com</a></p>\
+						</div>\
+					</div>\
+					<!-- Single Footer -->\
+\
+					<!-- Single Footer -->\
+				</div>\
+			</div>\
+		</footer>\
+\
+		<!-- Copyright -->\
+		<div class="copyright">\
+				<div class="container">\
+					<div class="row">\
+						<div class="col-sm-4">\
+							<div class="copy-text">\
+								<p>Copyright 2017 © Nickel Management LLC</p> \
+                                <p>Website by <strong><a href="http://niklaspeters.com" target="_blank">Niklas Peters</a></strong></p>\
+							</div>\
+						</div>\
+						<div class="col-sm-5">\
+							<div class="footer-menu pull-right">\
+								<ul>\
+									<li><a href="../services.html">Our Services</a></li>\
+									<li><a href="../rentals.html">List Your Rental</a></li>\
+									<li><a href="../contact.html">Contact Us</a></li>\
+								</ul>\
+							</div>\
+						</div>\
+						<div class="col-sm-3">\
+							<div class="social">\
+								<ul>\
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>\
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>\
+								</ul>\
+							</div>\
+						</div>\
+					</div>\
+				</div>\
+			</div>\
+\
+');
---
+++ menu.js
@@ -0,0 +1,30 @@
+document.write('\
+			<div class="main-menu">\
+				<div class="navbar-wrapper">\
+					<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">\
+						<div class="container">\
+						<img class="logo" src="img/nickel-logo-100.png" width="50" height="50">\
+							<div class="navbar-header">\
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">\
+									<span class="sr-only">Toggle Navigation</span>\
+									<span class="icon-bar"></span>\
+									<span class="icon-bar"></span>\
+									<span class="icon-bar"></span>\
+								</button>\
+\
+<a href="../index.html" class="navbar-brand"><h1>Nickel Management</h1></a>\
+							</div>\
+\
+							<div class="navbar-collapse collapse">\
+								<ul class="nav navbar-nav navbar-right">\
+									<li><a href="../services.html">Our Services</a></li>\
+									<li><a href="../rentals.html">List Your Rental</a></li>\
+									<li><a href="../contact.html">Contact Us</a></li>\
+\
+								</ul>\
+							</div>\
+						</div>\
+					</div>\
+				</div>\
+			</div>\
+');
